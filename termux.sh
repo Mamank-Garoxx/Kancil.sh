f2893c8048dd90b2a753b70037e7d1af12ce338b
Shark-ttack <50921785+Shark-ttack@users.noreply.github.com>
2019-06-16 01:30:37 +0000

    Add files via upload

---
+++ termux.sh
@@ -0,0 +1,3 @@
+z="
+";Az='pkg ';Gz='e';Hz='gem ';Lz='at';Mz='curl';Ez='g up';Fz='grad';Kz='lolc';Bz='upda';Dz='& pk';Iz='inst';Jz='all ';Cz='te &';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$z$Hz$Iz$Jz$Kz$Lz$z$Az$Iz$Jz$Kz$Lz$z$Az$Iz$Jz$Mz"
